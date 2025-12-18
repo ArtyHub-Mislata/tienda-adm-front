fcb051eee18e5f516ebf640ad9aad1558c063aef
luismi <olmedaromeroluismiguel@gmail.com>
2025-12-18 08:53:57 +0100

    Minicommit añadido el barer al interceptor

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -10,7 +10,7 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   if (token) {
     newReq = req.clone({
       setHeaders: {
-        Authorization: token
+        Authorization: `Bearer ${token}`
       }
     });
   }
